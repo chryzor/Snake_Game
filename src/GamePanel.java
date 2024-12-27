9a594ab7a9f421d144a500f8f6ca8d2dc56472e0
Zimani-Z <113458804+Zimani-Z@users.noreply.github.com>
2024-12-27 15:06:15 -0500

    New code

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -45,6 +45,7 @@ public class GamePanel extends JPanel implements ActionListener {
         startGame();
 
     }
+    //How or fast the game will be running in 
     public void startGame(){
         newApple();
         running = true;
@@ -53,12 +54,23 @@ public class GamePanel extends JPanel implements ActionListener {
 
     }
     public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        draw(g);
 
     }
     public void draw(Graphics g){
 
+        for(int i=0;i<SCREEN_HEIGHT/UNIT_SIZE;i++) {
+            g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+            g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
+        
+    }
+    g.setColor(Color.red);
+    g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
     }
     public void newApple(){
+        appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+        appleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
 
     }
     public void move(){
